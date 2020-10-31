5a9e70b941d16fa94a90b6ec7f5429f47417c960
Juan David Marín <wvany1996@gmail.com>
2020-10-31 04:04:12 -0500

    visual

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import FavoritesEmptyState from './FavoritesEmptyState';
+import Colors from 'cryptoTracker/src/res/colors';
 
 class FavoritesScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Favorites Screen</Text>
+      <View style={styles.container}>
+        <FavoritesEmptyState />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.charade,
+    flex: 1,
+    
+  }
+})
+
 export default FavoritesScreen;
--- src/components/favorites/FavoritesStack.js
+++ src/components/favorites/FavoritesStack.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import FavoritesScreen from './FavoritesScreen';
+import Colors from 'cryptoTracker/src/res/colors';
 
 const Stack = createStackNavigator();
 
 const FovoritesStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.blackPearl,
+          shadowColor: Colors.blackPearl,
+        },
+        headerTintColor: Colors.white,
+      }}>
       <Stack.Screen name="Fovorites" component={FavoritesScreen} />
     </Stack.Navigator>
   );
